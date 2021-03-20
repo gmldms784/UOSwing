9c807f79b4116bfb225ec1b7b69ed19ea4fc075b
kir3i <kirei7081@gmail.com>
2021-03-20 22:59:11 +0900

    backend 초기 커밋

---
+++ backend/src/test/java/uos/samsam/wing/WingApplicationTests.java
@@ -0,0 +1,13 @@
+package uos.samsam.wing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
