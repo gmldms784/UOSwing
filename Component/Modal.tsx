aeb5012edbc1ff8cc616ab9fe6e7efb03b9f41d7
heeeun <gmldms784@naver.com>
2021-04-03 15:34:43 +0900

    add modal background

--- Component/Modal.tsx
+++ Component/Modal.tsx
@@ -6,6 +6,7 @@ import {
 	Text,
 	TouchableHighlight
 } from 'react-native';
+import { borderColor } from '../StyleVariable';
 
 type Props = {
 	view : boolean
@@ -18,44 +19,71 @@ const Modal : React.FC<Props> = ({children, view, onClose, title}) => {
 		return null;
 	return (
 		<View style={ModalStyle.wrap}>
-			<View style={ModalStyle.header}>
-				<Text style={ModalStyle.title}>{title}</Text>
-				<TouchableHighlight
-					style={ModalStyle.xbtn}
-					onPress={onClose}
-					underlayColor="transparent"
-				>
-					<Text style={{
-						fontSize: 18
-					}}>X</Text>
-				</TouchableHighlight>
+			<View style={ModalStyle.back}></View>
+			<View style={ModalStyle.modal}>
+				<View style={ModalStyle.header}>
+					<Text style={ModalStyle.title}>{title}</Text>
+					<TouchableHighlight
+						style={ModalStyle.xbtn}
+						onPress={onClose}
+						underlayColor="transparent"
+					>
+						<Text style={{
+							fontSize: 18
+						}}>X</Text>
+					</TouchableHighlight>
+				</View>
+				{children}
 			</View>
-			{children}
 		</View>
 	);
 };
 
 const ModalStyle = StyleSheet.create({
-	wrap : {
+	wrap: {
 		position: "absolute",
-		top: "30%",
-		zIndex: 10,
+		top: 0,
+		left: 0,
+		width: "100%",
+		height: "100%",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center"
+	},
+	back: {
+		position: "absolute",
+		width: "100%",
+		height: "100%",
+		backgroundColor: "black",
+		opacity: 0.5,
+		zIndex: 11,
+	},
+	modal : {
+		zIndex: 12,
 		backgroundColor: "white",
-		padding: 20
+		padding: 20,
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 3,
+		},
+		shadowOpacity: 1,
+		shadowRadius: 4,
+		elevation: 6,
 	},
 	header : {
 		display: "flex",
 		justifyContent: "space-between",
 		flexDirection: "row",
-		alignItems: "center"
+		alignItems: "center",
 	},
 	title : {
 		fontSize: 16,
-		fontFamily: 'DOHYEON'
+		fontFamily: 'DOHYEON',
 	},
 	xbtn : {
 		marginLeft: 15,
-		marginRight: 5
+		marginRight: 5,
 	}
 })
 
