5856da991c9be774e11b27020563ab253bc10ed2
heeeun <gmldms784@naver.com>
2021-04-05 11:44:33 +0900

    date error resolve

--- Function/DateToString.ts
+++ Function/DateToString.ts
@@ -1,7 +1,7 @@
 export const dateToString = (date : Date) => {
-	const year = date.getFullYear();
-	let month = date.getMonth().toString();
-	let day= date.getDay().toString();
+	const year = date.getUTCFullYear();
+	let month = (date.getUTCMonth()+1).toString();
+	let day= date.getUTCDate().toString();
 
 	month = month.length === 1 ? "0" + month : month;
 	day = day.length === 1 ? "0" + day : day;
