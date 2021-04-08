7a3d8ad10455d53609dc33d6f61869d28b298bcf
MOBUMIN <kiju23@naver.com>
2021-04-08 21:18:04 +0900

    Setting Modal 제거
    * SettingModal 제거
    * ScrollView이슈 해결

--- Screen/SettingScreen.tsx
+++ Screen/SettingScreen.tsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import {
   ScrollView,
-  Text
+  Text,
+  TextInput,
+  View,
+  StyleSheet
 } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 
 import { usePadBoxState } from '../Main/Model/PadBoxModel'
 import { padBoxType } from '../Main/Type';
-import { SettingCard, SettingModal } from '../Component';
+import { Logotitle, Modal, SettingCard, ButtonComponent } from '../Component';
 import SquareIcon from '../assets/square.svg';
 import { SettingStackParamList } from '../Router/SettingRouter';
+import { TouchableHighlight } from 'react-native-gesture-handler';
 
 type Props = {
 	navigation: StackNavigationProp<SettingStackParamList, 'SettingCreate'>;
@@ -18,6 +22,8 @@ type Props = {
 const SettingScreen = ({ navigation } : Props) => {
 	const settingData = usePadBoxState();
 	const [modal, setModal] = useState<boolean>(false);
+	const [name, setName] = useState<string>("");
+	const [pos, setPos] = useState<string>("");
 
 	const handleModalOpen = () => {
 		setModal(true);
@@ -25,10 +31,9 @@ const SettingScreen = ({ navigation } : Props) => {
 	const handleModalClose = () => {
 		setModal(false);
 	}
-
 	return (
 		<>
-			<ScrollView>
+			<ScrollView contentContainerStyle={{flexGrow:1}}>
 				{
 					settingData.map((setting: padBoxType, index: number) => 
 						<>
@@ -42,14 +47,32 @@ const SettingScreen = ({ navigation } : Props) => {
 								temperature={setting.temperature}
 								modalOpen={handleModalOpen}
 							/>
-							<SettingModal
+							<Modal
 								view={modal}
 								onClose={handleModalClose}
-								icon={<SquareIcon width={30} height={30} fill="black" />}
-								title="개별 생리대함 관리"
-								initialName={setting.name}
-								initialPos={setting.address}
-							/>
+								title={<Logotitle icon={<SquareIcon width={30} height={30} fill="black" />} name="생리대함 생성" />}
+							>
+								<View style={{ width: 270 }}>
+									<Text style={MS.title}>이름</Text>
+									<TextInput value={name} onChangeText={setName} style={MS.input} />
+									<Text style={MS.title}>장소</Text>
+									<TextInput value={pos} onChangeText={setPos} style={MS.input} />
+									<TouchableHighlight
+										// !키보드가 올라오면 버튼이 자리를 벗어남 해결필요!
+										style={{
+											width: "50%",
+											left: "25%",
+											marginTop: 20
+										}}
+										underlayColor="transparent"
+										onPress={handleModalClose} // todo
+									>
+										<ButtonComponent color="mint">
+											<Text style={MS.btnText}>완료</Text>
+										</ButtonComponent>
+									</TouchableHighlight>
+								</View>
+							</Modal>
 						</>
 					)
 				}
@@ -57,5 +80,27 @@ const SettingScreen = ({ navigation } : Props) => {
 		</>
 	);
 }
+const MS = StyleSheet.create({
+	title: {
+		paddingLeft: 10,
+		marginTop: 25,
+		borderLeftColor: 'black',
+		borderLeftWidth: 3,
+		fontSize: 18,
+		fontWeight: '600',
+		fontFamily: 'DOHYEON',
+	},
+	input: {
+		borderWidth: 1,
+		borderRadius: 7,
+		padding: 5,
+		marginTop: 10,
+	},
+	btnText: {
+		fontSize: 15,
+		fontFamily: 'DOHYEON',
+		marginVertical: 7,
+	}
+})
 
 export default SettingScreen;
