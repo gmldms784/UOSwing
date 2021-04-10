84ada69b85bfb630d78ca2bc813bef35cc28fb43
MOBUMIN <kiju23@naver.com>
2021-04-10 15:40:18 +0900

    Update SettingScreen.tsx
    modal 따로 뺴서 정보 세팅

--- Screen/SettingScreen.tsx
+++ Screen/SettingScreen.tsx
@@ -1,4 +1,4 @@
-import React, { EventHandler, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   ScrollView,
   Text,
@@ -25,7 +25,9 @@ const SettingScreen = ({ navigation } : Props) => {
 	const [name, setName] = useState<string>("");
 	const [pos, setPos] = useState<string>("");
 
-	const handleModalOpen = () => {
+	const handleModalOpen = (n:string, a:string) => {
+		setName(n);
+		setPos(a);
 		setModal(true);
 	}
 	const handleModalClose = () => {
@@ -36,48 +38,46 @@ const SettingScreen = ({ navigation } : Props) => {
 			<ScrollView contentContainerStyle={{flexGrow:1}}>
 				{
 					settingData.map((setting: padBoxType, index: number) => 
-						<>
-							<SettingCard
-								key={setting.id}
-								index={index}
-								name={setting.name}
-								address={setting.address}
-								padAmount={setting.padAmount}
-								humidity={setting.humidity}
-								temperature={setting.temperature}
-								modalOpen={handleModalOpen}
-							/>
-							<Modal
-								view={modal}
-								onClose={handleModalClose}
-								title={<Logotitle icon={<SquareIcon width={30} height={30} fill="black" />} name="개별 생리대함 관리" />}
-							>
-								{/* 이름, 장소 받아오기 구현해야함, 어떻게..?????  */}
-								{/* modal이 겹쳐 뜨는 것 같음 */}
-								<View style={{ width: 270 }}>
-									<Text style={MS.title}>이름</Text>
-									<TextInput value={setting.name} style={MS.input} />
-									<Text style={MS.title}>장소</Text>
-									<TextInput value={setting.address} style={MS.input} />
-									<TouchableHighlight
-										// !키보드가 올라오면 버튼이 자리를 벗어남 해결필요!
-										style={{
-											width: "50%",
-											left: "25%",
-											marginTop: 20
-										}}
-										underlayColor="transparent"
-										onPress={handleModalClose} // todo
-									>
-										<ButtonComponent color="mint">
-											<Text style={MS.btnText}>완료</Text>
-										</ButtonComponent>
-									</TouchableHighlight>
-								</View>
-							</Modal>
-						</>
+						<SettingCard
+							key={setting.id}
+							index={index}
+							name={setting.name}
+							address={setting.address}
+							padAmount={setting.padAmount}
+							humidity={setting.humidity}
+							temperature={setting.temperature}
+							modalOpen={()=>handleModalOpen(setting.name, setting.address)}
+						/>
 					)
 				}
+				<Modal
+					view={modal}
+					onClose={handleModalClose}
+					title={<Logotitle icon={<SquareIcon width={30} height={30} fill="black" />} name="개별 생리대함 관리" />}
+				>
+					{/* 이름, 장소 받아오기 구현해야함, 어떻게..?????  */}
+					{/* modal이 겹쳐 뜨는 것 같음 */}
+					<View style={{ width: 270 }}>
+						<Text style={MS.title}>이름</Text>
+						<TextInput value={name} onChangeText={setName} style={MS.input} />
+						<Text style={MS.title}>장소</Text>
+						<TextInput value={pos} onChangeText={setPos} style={MS.input} />
+						<TouchableHighlight
+							// !키보드가 올라오면 버튼이 자리를 벗어남 해결필요!
+							style={{
+								width: "50%",
+								left: "25%",
+								marginTop: 20
+							}}
+							underlayColor="transparent"
+							onPress={handleModalClose} // todo
+						>
+							<ButtonComponent color="mint">
+								<Text style={MS.btnText}>완료</Text>
+							</ButtonComponent>
+						</TouchableHighlight>
+					</View>
+				</Modal>
 			</ScrollView>
 		</>
 	);
