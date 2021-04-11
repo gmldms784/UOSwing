12b330fcb77251834d16bdab9194cd1c72a8182e
heeeun <gmldms784@naver.com>
2021-04-11 21:42:54 +0900

    fix datetostring error

--- Function/DateToString.ts
+++ Function/DateToString.ts
@@ -1,7 +1,7 @@
 export const dateToString = (date : Date) => {
 	const year = date.getFullYear();
-	let month = date.getMonth().toString();
-	let day= date.getDay().toString();
+	let month = (date.getMonth()+1).toString();
+	let day= date.getDate().toString();
 
 	month = month.length === 1 ? "0" + month : month;
 	day = day.length === 1 ? "0" + day : day;
