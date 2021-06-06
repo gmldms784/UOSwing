51e5936b2942b77c83a4f84ce55b8b7c7df379ea
gmldms784 <gmldms784@naver.com>
2021-06-06 11:45:38 +0900

    마커 크기 조정

--- Component/MarkerComponent.tsx
+++ Component/MarkerComponent.tsx
@@ -44,7 +44,6 @@ const MarkerComponent = ({number, name, address, latitude, longitude, amount, is
 					latitude: latitude,
 					longitude: longitude
 				}}
-				style={{ padding: 10 }}
 				onPress={() => onPress(name, address)}
 			>
 				{
@@ -69,10 +68,11 @@ const MarkerComponent = ({number, name, address, latitude, longitude, amount, is
 
 const MarkerStyle = StyleSheet.create({
 	marker2: {
-		padding: 11,
+		padding: 15,
 		borderRadius: 15,
-		borderColor: 'black',
-		borderWidth:1,
+		borderColor: '#535353',
+		borderWidth:3,
+		margin: 5
 	},
 	marker: {
 		padding: 20,
